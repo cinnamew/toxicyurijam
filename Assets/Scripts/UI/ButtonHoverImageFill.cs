be01195f6cb76d6de0281c28a5b4b59962bac0c8
kongshijatwit <JKongShi@gmail.com>
2026-06-20 19:28:25 -0400

    Expose fill time values to the inspector

--- Assets/Scripts/UI/ButtonHoverImageFill.cs
+++ Assets/Scripts/UI/ButtonHoverImageFill.cs
@@ -5,14 +5,14 @@ using DG.Tweening;
 
 public class ButtonHoverImageFill : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    private const float IMAGE_FILL_DURATION_SECONDS = 0.5f;
+    [SerializeField] private float timeToFill;
     [SerializeField] private Image[] fillImages;
 
     public void OnPointerEnter(PointerEventData eventData)
     {
         foreach (Image image in fillImages)
         {
-            image.DOFillAmount(1, IMAGE_FILL_DURATION_SECONDS);
+            image.DOFillAmount(1, timeToFill);
         }
     }
 
@@ -20,7 +20,7 @@ public class ButtonHoverImageFill : MonoBehaviour, IPointerEnterHandler, IPointe
     {
         foreach (Image image in fillImages)
         {
-            image.DOFillAmount(0, IMAGE_FILL_DURATION_SECONDS);
+            image.DOFillAmount(0, timeToFill);
         }
     }
 }
--- Assets/Scripts/UI/TextHoverColorTween.cs
+++ Assets/Scripts/UI/TextHoverColorTween.cs
@@ -1,12 +1,10 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 using DG.Tweening;
-using TMPro;
 using UnityEngine.UI;
 
 public class TextHoverColorTween : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler 
 {
-    private const float TEXT_FILL_DURATION_SECONDS = 0.75f;
     [SerializeField] private Graphic originalText;
     [SerializeField] private Color targetColor;
     [SerializeField] private float timeToFill;
@@ -14,40 +12,16 @@ public class TextHoverColorTween : MonoBehaviour, IPointerEnterHandler, IPointer
 
     private void Start()
     {
-        // if (originalText is TextMeshProUGUI)
-        // {
-        //     originalColor = (originalText as TextMeshProUGUI).color;
-        // }
-        // else if (originalText is Text)
-        // {
-        //     originalColor = (originalText as Text).color;
-        // }
         originalColor = originalText.color;
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        // if (originalText is TextMeshProUGUI)
-        // {
-        //     (originalText as TextMeshProUGUI).DOColor(targetColor, TEXT_FILL_DURATION_SECONDS);
-        // }
-        // else if (originalText is Text)
-        // {
-        //     (originalText as Text).DOColor(targetColor, TEXT_FILL_DURATION_SECONDS);
-        // }
         originalText.DOColor(targetColor, timeToFill);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        // if (originalText is TextMeshProUGUI)
-        // {
-        //     (originalText as TextMeshProUGUI).DOColor(originalColor, TEXT_FILL_DURATION_SECONDS);
-        // }
-        // else if (originalText is Text)
-        // {
-        //     (originalText as Text).DOColor(originalColor, TEXT_FILL_DURATION_SECONDS);
-        // }
         originalText.DOColor(originalColor, timeToFill);
     }
 }
