7d3838b8d9109996b3e0151571c9d6b35c4d0207
kongshijatwit <JKongShi@gmail.com>
2026-06-21 16:21:02 -0400

    Fix inventory slot layout

--- Assets/Scripts/Inventory/InventorySlotButton.cs
+++ Assets/Scripts/Inventory/InventorySlotButton.cs
@@ -1,10 +1,25 @@
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public class InventorySlotButton : MonoBehaviour, ISelectHandler
 {
+    private Image itemImage;
+
+    private void Start()
+    {
+        itemImage = transform.GetChild(0).GetComponent<Image>();
+    }
+
     public void OnSelect(BaseEventData eventData)
     {
         InventoryManager.Instance.SetCurrentItem(transform.GetSiblingIndex());
+    }
+
+    public void SetSprite(Sprite sprToSet)
+    {
+        itemImage.sprite = sprToSet;
+        itemImage.color = sprToSet == null ? Color.clear : Color.white;
+        itemImage.preserveAspect = true;
     }
 }
--- Assets/Scripts/Inventory/InventoryUI.cs
+++ Assets/Scripts/Inventory/InventoryUI.cs
@@ -3,6 +3,15 @@ using UnityEngine.UI;
 
 public class InventoryUI : MonoBehaviour
 {
+    [SerializeField] private HorizontalLayoutGroup hotbar;
+    private InventorySlotButton[] hotbarImages;
+
+
+    private void Start()
+    {
+        hotbarImages = hotbar.GetComponentsInChildren<InventorySlotButton>();
+    }
+
     void OnEnable()
     {
         Inventory.OnInventoryChanged += UpdateInventoryDisplay;
@@ -17,22 +26,14 @@ public class InventoryUI : MonoBehaviour
     {
         for (int i = 0; i < inventory.Length; i++)
         {
-            Debug.Log(inventory[i].Id);
             if (inventory[i].Id != "nullobj")
             {
-                SetSprite(transform.GetChild(i).GetChild(0).GetComponent<Image>(), inventory[i].ItemObject.Icon);
+                hotbarImages[i].SetSprite(inventory[i].ItemObject.Icon);
             }
             else
             {
-                SetSprite(transform.GetChild(i).GetChild(0).GetComponent<Image>(), null);
+                hotbarImages[i].SetSprite(null);
             }
         }
-    }
-
-    private void SetSprite(Image imgToSet, Sprite sprToSet)
-    {
-        imgToSet.sprite = sprToSet;
-        imgToSet.color = sprToSet == null ? Color.clear : Color.white;
-        imgToSet.preserveAspect = true;
     }
 }
