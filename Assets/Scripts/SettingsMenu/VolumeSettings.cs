6516e05299dc32244cf6e2644764c3d9d56d13cc
kongshijatwit <JKongShi@gmail.com>
2026-07-01 14:16:22 -0400

    Add volume slider scripts to settings

--- Assets/Scripts/SettingsMenu/VolumeSettings.cs
+++ Assets/Scripts/SettingsMenu/VolumeSettings.cs
@@ -5,34 +5,23 @@ using UnityEngine.UI;
 public class VolumeSettings : MonoBehaviour
 {
     [SerializeField] private AudioMixer mainMixer;
-    [SerializeField] private Slider masterSlider, musicSlider, sfxSlider;
+    [SerializeField] private Slider musicSlider, sfxSlider;
 
     private void Start()
     {
-        masterSlider.value = PlayerPrefs.GetFloat(Globals.MASTER_VOLUME, 1f);
-        musicSlider.value = PlayerPrefs.GetFloat(Globals.MUSIC_VOLUME, 1f);
-        sfxSlider.value = PlayerPrefs.GetFloat(Globals.SFX_VOLUME, 1f);
+        SetupSlider(musicSlider, Globals.MUSIC_VOLUME);
+        SetupSlider(sfxSlider, Globals.SFX_VOLUME);
+    }
 
-        mainMixer.SetFloat(Globals.MASTER_VOLUME, 20 * Mathf.Log10(masterSlider.value + float.Epsilon));
-        mainMixer.SetFloat(Globals.MUSIC_VOLUME, 20 * Mathf.Log10(musicSlider.value + float.Epsilon));
-        mainMixer.SetFloat(Globals.SFX_VOLUME, 20 * Mathf.Log10(sfxSlider.value + float.Epsilon));
+    private void SetupSlider(Slider slider, string globalReference)
+    {
+        slider.value = PlayerPrefs.GetFloat(globalReference, 1f);
+        mainMixer.SetFloat(globalReference, 20 * Mathf.Log10(slider.value + float.Epsilon));
 
-        masterSlider.onValueChanged.AddListener(val =>
+        slider.onValueChanged.AddListener(val =>
         {
-            mainMixer.SetFloat(Globals.MASTER_VOLUME, 20 * Mathf.Log10(val + float.Epsilon));
-            PlayerPrefs.SetFloat(Globals.MASTER_VOLUME, masterSlider.value);
-        });
-
-        musicSlider.onValueChanged.AddListener(val =>
-        {
-            mainMixer.SetFloat(Globals.MUSIC_VOLUME, 20 * Mathf.Log10(val + float.Epsilon));
-            PlayerPrefs.SetFloat(Globals.MUSIC_VOLUME, musicSlider.value);
-        });
-
-        sfxSlider.onValueChanged.AddListener(val =>
-        {
-            mainMixer.SetFloat(Globals.SFX_VOLUME, 20 * Mathf.Log10(val + float.Epsilon));
-            PlayerPrefs.SetFloat(Globals.SFX_VOLUME, sfxSlider.value);
+            mainMixer.SetFloat(globalReference, 20 * Mathf.Log10(val + float.Epsilon));
+            PlayerPrefs.SetFloat(globalReference, slider.value);
         });
     }
 }
