16592d06acf98a1a4cb899427b9628418d31d090
cinnamew <jolie.han@gmail.com>
2026-07-09 13:13:58 -0700

    creds or smth + ending ,, i'm sorry i am rewriting u jonathan

---
+++ Assets/Scripts/Menus/Credits.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.UI;
+using Fungus;
+
+public class Credits : MonoBehaviour
+{
+    [SerializeField] private VerticalLayoutGroup creditsContainer;
+    [SerializeField] private GameObject returnToTitleButton;
+    [SerializeField] private Flowchart flowchart;
+
+    private int nextChildIndex;
+    private bool finished;
+
+    private void Start()
+    {
+        foreach (Transform child in creditsContainer.transform) {
+            child.gameObject.SetActive(false);
+        }
+
+        returnToTitleButton.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (finished || !Input.GetMouseButtonDown(0)) {
+            return;
+        }
+
+        AdvanceCredits();
+    }
+
+    private void AdvanceCredits()
+    {
+        Transform container = creditsContainer.transform;
+
+        if (nextChildIndex < container.childCount)
+        {
+            container.GetChild(nextChildIndex).gameObject.SetActive(true);
+            nextChildIndex++;
+            return;
+        }
+
+        if (returnToTitleButton != null) returnToTitleButton.SetActive(true);
+        
+        flowchart.ExecuteBlock("end of game");
+
+        finished = true;
+    }
+}
