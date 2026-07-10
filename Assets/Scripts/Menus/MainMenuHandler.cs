9afb0d6272941a12fe5a1811365996cd11d9319d
kongshijatwit <JKongShi@gmail.com>
2026-07-09 23:11:44 -0400

    Finish main menu

--- Assets/Scripts/Menus/MainMenuHandler.cs
+++ Assets/Scripts/Menus/MainMenuHandler.cs
@@ -1,3 +1,4 @@
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -9,10 +10,27 @@ public class MainMenuHandler : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    public void ChapterSelectButtonPressed()
+    {
+        if (UtilityMenuHandler.Instance != null)
+            UtilityMenuHandler.Instance.OpenToTab(1);
+    }
+
     public void SettingsButtonPressed()
     {
         if (UtilityMenuHandler.Instance != null)
             UtilityMenuHandler.Instance.OpenToTab(0);
+    }
+
+    public void ExitGameButtonPressed()
+    {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#elif UNITY_WEBGL
+        Debug.Log("Who gave you permission to leave?");
+#else
+        Application.Quit();
+#endif
     }
 
     public void MainMenuPressed()
