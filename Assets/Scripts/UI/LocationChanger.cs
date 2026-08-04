191f5fdaefa39301523cf1bf2f4979ab9895683a
cinnamew <101888513+cinnamew@users.noreply.github.com>
2026-08-03 21:20:39 -0700

    it's not like i need you anyways
    HOLY I KEEP PYTHONINGGG

--- Assets/Scripts/UI/LocationChanger.cs
+++ Assets/Scripts/UI/LocationChanger.cs
@@ -1,5 +1,8 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.Localization.Settings;
+using System.Collections;
+using UnityEngine.Localization.Tables;
 
 public class LocationChanger : MonoBehaviour
 {
@@ -7,11 +10,34 @@ public class LocationChanger : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        if (text != null)
+        {
+            ChangeLocationText(text.text);
+            //Debug.Log("meow");
+        }
     }
 
     public void ChangeLocationText(string newLoc)
     {
-        text.text = newLoc;
+        StartCoroutine(LocalizeLocation(newLoc));
+    }
+
+    public IEnumerator LocalizeLocation(string newLoc)
+    {
+        string key = "Location " + newLoc;
+        var tableOperation = LocalizationSettings.StringDatabase.GetTableAsync("NonDialogueText");
+        yield return tableOperation;
+
+        StringTable table = tableOperation.Result;
+        StringTableEntry entry = table.GetEntry(key);
+        if (entry == null)
+        {
+            Debug.Log("localization key for " + newLoc + " doesn't exist :(((");
+            text.text = "?";
+            yield break;
+        }
+        //LocalizationSettings.StringDatabase.GetLocalizedString("NonDialogueText", key);
+        string newText = entry.GetLocalizedString();
+        text.text = newText;
     }
 }
